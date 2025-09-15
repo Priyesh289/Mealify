5bd1b8137510f715555d239b410bc8f429baf16b
Priyesh Shukla <152530125+Priyesh289@users.noreply.github.com>
2025-09-15 21:23:33 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeholder = async (req, res) => {
 
-    const frontend_url = 'http://localhost:5173'
+    const frontend_url = 'https://mealify-frontend.onrender.com/'
     try {
         const newOrder = new orderModel({
             userId: req.userId,
